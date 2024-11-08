d41102760662a718f83208170eab5caa1f0cb1d7
Ramel <dev_20@naver.com>
2024-11-09 00:27:17 +0900

    [Add: 배너 기능]

---
+++ src/main/java/cafeSubscription/coffee/domain/Image/DTO/ImageResponse.java
@@ -0,0 +1,15 @@
+package cafeSubscription.coffee.domain.Image.DTO;
+
+import cafeSubscription.coffee.domain.Image.Image;
+import lombok.Getter;
+
+@Getter
+public class ImageResponse {
+    private final Integer imageId;
+    private final String imagePath;
+
+    public ImageResponse(Image image) {
+        this.imageId = image.getImageId();
+        this.imagePath = image.getImagePath();
+    }
+}
